ef9faac0a20fc806d42c3b0d9f7256329af99eff
ns0304 <ns0304@naver.com>
2024-11-09 22:27:17 +0900

    [Bronze IV] Title: 별 찍기 - 2, Time: 276 ms, Memory: 18368 KB -BaekjoonHub

--- 백준/Bronze/2439. 별 찍기 － 2/별 찍기 － 2.java
+++ 백준/Bronze/2439. 별 찍기 － 2/별 찍기 － 2.java
@@ -5,14 +5,14 @@ public class Main {
 
         int N = sc.nextInt();
 
-        for(int i = 1; i <= N; i++) {		// 행 반복
-            for(int j = 1; j <= N - i; j++){	// 공백 반복
+        for(int i = 1; i <= N; i++) {		
+            for(int j = 1; j <= N - i; j++){	
                 System.out.print(" ");
             }
-            for(int k = 1; k <= i; k++){	// 별 반복
+            for(int k = 1; k <= i; k++){
                 System.out.print("*");
             }
-            System.out.println();		// 줄 바꿈
+            System.out.println();		
         }
     }
 }
